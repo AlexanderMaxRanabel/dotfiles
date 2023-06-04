8f293dc97c408980158a478cd478290782dcc34a
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-06-04 09:55:35 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,3 @@
-# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -114,10 +113,8 @@
      helix
      fish
      gcc
-     librewolf
      rustup
-     nushell
-     cmatrix
+     openssl
      #librewolf-unwrapped
      #chromium
      #brave
